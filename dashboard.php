ad96c575552503e225a2afba96d1452a04ae1dce
Muhammad Firdaus <muhammad.firdaus6958@icloud.com>
2026-06-12 05:43:08 +0800

    Update dashboard dan index

--- dashboard.php
+++ dashboard.php
@@ -1,21 +1,12 @@
 <?php
 
 $conn = mysqli_connect("localhost", "root", "", "nama_database");
-$sql = "SELECT full_name, job_title, location FROM applications";
+
+$sql = "SELECT application_id, full_name, job_title, location
+        FROM applications";
+
 $result = mysqli_query($conn, $sql);
 
+$applications = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
 ?>
-
-<div class="grid">
-
-<?php while($row = mysqli_fetch_assoc($result)): ?>
-
-    <div class="card">
-        <h2><?php echo $row['full_name']; ?></h2>
-        <p><?php echo $row['job_title']; ?></p>
-        <p><?php echo $row['location']; ?></p>
-    </div>
-
-<?php endwhile; ?>
-
-</div>
