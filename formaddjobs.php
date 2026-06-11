c4516822a620bd03cd2d7fac8b35cd68d43b6f4a
Nurulain Nabilah <nrulainnabilah@gmail.com>
2026-06-12 04:23:56 +0800

    rename and update formaddjobs.php

--- formaddjobs.php
+++ formaddjobs.php
@@ -1,68 +1,89 @@
+<?php
+session_start();
+
+if ($_SERVER["REQUEST_METHOD"] == "POST")
+{
+    $job = $_POST['job'];
+    $location = $_POST['location'];
+    $start_date = $_POST['start_date'];
+    $end_date = $_POST['end_date'];
+    $allowance = $_POST['allowance'];
+    $todo = $_POST['todo'];
+
+    if (!isset($_SESSION['jobs'])) {
+        $_SESSION['jobs'] = [];
+    }
+
+    $_SESSION['jobs'][] = [
+        "job" => $job,
+        "location" => $location,
+        "start_date" => $start_date,
+        "end_date" => $end_date,
+        "allowance" => $allowance,
+        "todo" => $todo
+    ];
+
+    header("Location: page6admin.php");
+    exit();
+}
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
     <title>Add Jobs</title>
     <link rel="stylesheet" href="style.css">
-    <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
 </head>
 <body>
 
-    <div class="header">
-        <div>🎲 Fantastic5</div>
-        <div>📍 FTMK, Universiti Teknikal Malaysia Melaka</div>
-        <div>📞 +06 229 2121</div>
-        <div>✉ ftmk@utem.edu.my</div>
-    </div>
+<?php include 'header.php'; ?>
 
-    <div class="menu">
-        <a href="#">Dashboard</a>
-        <a href="#">Profile</a>
-        <a href="#">Jobs Available</a>
-    </div>
+<div class="container">
 
-    <div class="container">
+    <h1>Add Jobs</h1>
 
-        <h1>Add Jobs</h1>
+    <form method="post" onsubmit="return checkForm()">
 
-        <form action="page5admin.php" method="post" onsubmit="return checkForm()">
+        <div class="row">
+            <label>Job :</label>
+            <input type="text" id="job" name="job" required>
+        </div>
 
-            <div class="row">
-                <label>Job :</label>
-                <input type="text" id="job" name="job" required>
-            </div>
+        <div class="row">
+            <label>Location :</label>
+            <input type="text" id="location" name="location" required>
+        </div>
 
-            <div class="row">
-                <label>Location :</label>
-                <input type="text" id="location" name="location" required>
-            </div>
+        <div class="row">
+            <label>Start Date :</label>
+            <input type="date" id="start_date" name="start_date" required>
+        </div>
 
-            <div class="row">
-                <label>Start Date :</label>
-                <input type="date" id="start_date" name="start_date" required>
-            </div>
+        <div class="row">
+            <label>End Date :</label>
+            <input type="date" id="end_date" name="end_date" required>
+        </div>
 
-            <div class="row">
-                <label>End Date :</label>
-                <input type="date" id="end_date" name="end_date" required>
-            </div>
+        <div class="row">
+            <label>Allowance :</label>
+            <input type="text" id="allowance" name="allowance" required>
+        </div>
 
-            <div class="row">
-                <label>Allowance :</label>
-                <input type="text" id="allowance" name="allowance" required>
-            </div>
+        <div class="row">
+            <label>To Do :</label>
+            <input type="text" id="todo" name="todo" required>
+        </div>
 
-            <div class="row">
-                <label>To Do :</label>
-                <input type="text" id="todo" name="todo" required>
-            </div>
-
+        <div class="btn page5">
             <button type="submit">Submit</button>
+        </div>
 
-        </form>
+    </form>
 
-    </div>
+</div>
 
-    <script src="page5admin.js"></script>
+<script src="page5admin.js"></script>
 
 </body>
 </html>
