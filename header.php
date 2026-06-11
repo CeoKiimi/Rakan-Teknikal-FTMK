561e391ac2bfdb73f095b9b23373b938a5cd8842
Nurulain Nabilah <nrulainnabilah@gmail.com>
2026-06-12 04:26:35 +0800

    add header.php

---
+++ header.php
@@ -0,0 +1,20 @@
+    <link rel="stylesheet" href="style.css">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+
+
+    <div class="header">
+        <div>🎲 Fantastic5 </div>
+        <div>|</div>
+        <div>📍 FTMK, Universiti Teknikal Malaysia Melaka </div>
+        <div>|</div>
+        <div>📞 +06 229 2121 </div>
+        <div>|</div>
+        <div>✉ ftmk@utem.edu.my </div>
+    </div>
+
+        <div class="menu">
+        <!-- nnti tmbah dashboard.php -->
+        <a href="#">Dashboard</a>
+        <a href="#">Profile</a>
+        <a href="#">Jobs Available</a>
+    </div>
